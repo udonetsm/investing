f9905e63f5f64e7c65ccdaf4f50793ae0336c9da
Maksim Donets <maks.donets.95@outlook.com>
2024-02-08 13:23:59 +0300

    move saving transactions to the other package(calls) and clean superflous

--- actions/transactions/transactions.go
+++ actions/transactions/transactions.go
@@ -2,38 +2,20 @@
 package transactions
 
 import (
-	"fmt"
-	"strconv"
-	"time"
-
-	"github.com/udonetsm/investing/interfaces"
 	"github.com/udonetsm/investing/models"
 )
 
-func Transaction(from interfaces.Payer, to interfaces.Getter, sum float32) models.Transaction {
-	transaction_id, err := from.Pay(to, sum)
-	if err != nil {
-		return models.Transaction{}
+func TransferMoney(transaction models.Transaction) models.Transaction {
+	// Делаем оплату со счета отправителя
+	transaction = transaction.Payer.DoTransaction(transaction)
+	// Если в процессе перевода возникла проблема,
+	// то проверять баланс получателя незачем
+	// Просто возвращаем транзакцию с ошибкой
+	if transaction.Err != nil {
+		return transaction
 	}
-	err = to.Get(transaction_id)
-	if err != nil {
-		// Сообщение о неудаче
-		return models.Transaction{}
-	}
-	fmt.Println("Transaction OK")
-	// ВрЕменная генерация transaction_id, для тестирований
-	transaction_id = strconv.Itoa(int(time.Now().UnixNano()))
-	// Схранить транзакцию в базе, в таблице Transactions
-	return saveTransactios(transaction_id, from, to)
-}
-
-// Сохранить в базу информацию о транзакции
-func saveTransactios(transaction_id string, from interfaces.Payer, to interfaces.Getter) models.Transaction {
-	transaction := models.Transaction{
-		Transaction_id: transaction_id,
-		From:           from,
-		To:             to,
-	}
+	//Проверяем что получаеть получил необходимую сумму
+	// (нужная транзакция есть на счете получателя)
+	transaction = transaction.Reciever.Recieve(transaction)
 	return transaction
-	// Сохранить в базе, продумать формат хранения внутренних транзакций.
 }
