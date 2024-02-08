13342702d8c0f79bdc564eeeea5fc23bcb7aca43
Maksim Donets <maks.donets.95@outlook.com>
2024-02-08 13:21:33 +0300

    fix error capturing. Clean everything

--- actions/investor/investor.go
+++ actions/investor/investor.go
@@ -10,26 +10,35 @@ type Startuper models.Startupers
 
 // Investor может перевести деньги стартаперу.
 func (from Investor) DoTransaction(transaction models.Transaction) models.Transaction {
+	var err error
+	//err = Запросить баланс банковского счета отправителя
+	// и зарезервировать нужную сумму
 	// Если возникла какая-либо ошибка, транзакция не осуществляется
-	// и ошибка возвращается "наверх". Возможно не хватает средств или банк отклонил соединение
-	if transaction.Err != nil {
+	// и ошибка возвращается "наверх".
+	// Возможно не хватает средств или банк отклонил соединение
+	if err != nil {
 		// Ошибку проверки баланса можно кастомизировать здесь
-		return models.Transaction{Err: transaction.Err}
+		transaction.Err = err
+		return transaction
 	}
-	// Если средств достаточно и ошибки нет то запрашиваем у банка транзакцию.
-	if transaction.Err != nil {
-		// Неудача во время перевода
-		return models.Transaction{Err: transaction.Err}
+	// Если средств достаточно и ошибки нет то делаем транзакцию
+	// err:= Запрос внутренней транзакции
+	if err != nil {
+		transaction.Err = err
 	}
-	// Если все ок, то делаем лог и сообщение об успехе
-	// fmt.Printf("%v send %.2f to %v...", s, sum, to)
-	// И записываем транзакцию в базу, в таблицу Transactions
+	// Транзакция успешна. Раппортуем и
+	// записываем транзакцию в базу, в таблицу Transactions
 	return transaction
 }
 
 func (to Startuper) Recieve(transaction models.Transaction) models.Transaction {
-	if transaction.Err != nil {
-		return models.Transaction{Err: transaction.Err}
+	var err error
+	// err = Проверка пришли ли средства получателю
+	if err != nil {
+		// Деньги не пришли
+		transaction.Err = err
+		return transaction
 	}
+	//Деньги пришли
 	return transaction
 }
