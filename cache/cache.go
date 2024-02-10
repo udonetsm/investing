6d78f327f6875559459324497493ce4fe5e178fe
Maksim Donets <maks.donets.95@outlook.com>
2024-02-10 20:14:30 +0300

    add init function into the cache file, change type of cache. add method Save for cache which inserts record into curent transaction cache

--- cache/cache.go
+++ cache/cache.go
@@ -6,32 +6,35 @@ import (
 	"github.com/udonetsm/investing/models"
 )
 
-type TransactionCache struct {
-	Mux     *sync.Mutex
-	Storage map[string]models.Transaction
+type TransactionCache models.Cache
+
+var TC TransactionCache
+
+func init() {
+	TC = NewCache()
 }
 
-func NewCache() *TransactionCache {
-	return &TransactionCache{
+func NewCache() TransactionCache {
+	return TransactionCache{
 		Mux:     new(sync.Mutex),
-		Storage: NewStorage(),
+		Storage: NewStorage().(map[string]any),
 	}
 }
 
-func NewStorage() map[string]models.Transaction {
-	return make(map[string]models.Transaction)
+func NewStorage() any {
+	return make(map[string]any)
 }
 
-type Inserter interface {
-	Insert(string, models.Transaction)
-}
-
-func (t *TransactionCache) Insert(transaction models.Transaction) {
-	t.Mux.Lock()
-	defer t.Mux.Unlock()
-	if _, ok := t.Storage[transaction.Transaction_id]; !ok {
-		t.Storage[transaction.Transaction_id] = transaction
-	} else {
-		return
+// Private method
+func insertTransaction(c *TransactionCache, t any) error {
+	if c.Err != nil {
+		return c.Err
 	}
+	c.Storage[t.(models.Transaction).Transaction_id] = t.(models.Transaction)
+	return nil
+}
+
+// Делаю TransactionCache Saver-ом
+func (c *TransactionCache) Save(t any) (err error) {
+	return insertTransaction(c, t)
 }
