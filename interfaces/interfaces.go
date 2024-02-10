1b6414feb7260c6f96d0d7267c426e23496379a4
Maksim Donets <maks.donets.95@outlook.com>
2024-02-10 20:19:22 +0300

    move interfaces into the separate package.

--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -1,2 +1,11 @@
 // Interfaces
+// Several else are in models. Not here to avoid cycle import
 package interfaces
+
+type Saver interface {
+	Save(any) error
+}
+
+type BalanceUpdater interface {
+	BalanceUpdate(any) error
+}
