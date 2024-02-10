1305df7c24ee941d2100f3b78a42e7d77dd1c776
Maksim Donets <maks.donets.95@outlook.com>
2024-02-10 21:06:27 +0300

    add database package and init database testing database config

---
+++ database/database.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"github.com/udonetsm/investing/database/methods"
+)
+
+var DB methods.Database
+
+func init() {
+	DB = NewDatabase()
+}
+
+func NewDatabase() methods.Database {
+	return methods.Database{
+		User:    "Test2",
+		Pass:    "testpass",
+		Host:    "localhost",
+		Port:    5432,
+		SSLMode: false,
+	}
+}
