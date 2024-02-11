ac52e54492f4b065163b53f4aa758a02fd04edf2
Maksim Donets <maks.donets.95@outlook.com>
2024-02-11 20:05:49 +0300

    Enumerated available transactions. Commented it

---
+++ actions/transactioner/transactioner.go
@@ -0,0 +1,39 @@
+// All transactions
+package transactioner
+
+import (
+	"github.com/udonetsm/investing/models"
+)
+
+type System models.System
+
+// Первод между пользователями. Обязательно от инвестора стартаперу или обратно.
+// Переводы между инветорами запрещены.
+// Переводы между стартаперами запрещены.
+func (s *System) TransferTransaction(transaction *models.Transaction) {
+	if transaction.Err != nil {
+		return
+	}
+	// Делаем оплату со счета отправителя
+	transaction.Payer.Pay(transaction)
+	if transaction.Err != nil {
+		return
+	}
+	// Например недостаточно средств
+	if transaction.Err != nil {
+		return
+	}
+	transaction.Reciever.Recieve(transaction)
+}
+
+// Пополнение счета
+func (s *System) TopupTransaction(transaction *models.Transaction) {
+	transaction.Payer.Topup(transaction)
+	// ...
+}
+
+// Снятие средств со счета
+func (s *System) WithdrawTransaction(transaction *models.Transaction) {
+	transaction.Payer.Withdraw(transaction)
+	// ...
+}
