7ebd18b6af4ccf96057c165deee995161f91fee8
Maksim Donets <maks.donets.95@outlook.com>
2024-02-11 20:03:36 +0300

    add allowed methods for investor model. Investor now can Accept transaction, Request transaction, Pay, Recieve money, Topup its bill, Withdraw its bill. Comment it

--- actions/investor/investor.go
+++ actions/investor/investor.go
@@ -2,40 +2,59 @@
 package investor
 
 import (
+	"github.com/udonetsm/investing/actions/general"
 	"github.com/udonetsm/investing/models"
 )
 
 type Investor models.Investors
 type Startuper models.Startupers
 
-// Investor может перевести деньги стартаперу.
-func (from Investor) DoTransaction(transaction models.Transaction) models.Transaction {
-	// err:= проверяем баланс Payer-а
+// Перевести деньги стартаперу(на его внутренний счет).
+func (from *Investor) Pay(transaction *models.Transaction) {
 	if transaction.Err != nil {
-		// Ошибку проверки баланса можно кастомизировать здесь
-		return transaction
+		return
 	}
-	// Если средств достаточно и ошибки нет то делаем транзакцию
-	from.Bill.Balance -= transaction.Sum
-	transaction.Payer = from
-	return transaction
+	if transaction.Accepted {
+		from.Bill.Balance -= transaction.Transaction_sum
+	}
 }
 
-// Эта функция должна выполняться пока
-// на счете получателя не появится транзакция
-// Либо через done канал,
-// либо через wiatGroup,
-// либо через nats streaming server
-func (to Startuper) Recieve(transaction models.Transaction) models.Transaction {
-	var err error
-	// err = Проверка пришли ли средства получателю
-	if err != nil {
-		// Деньги не пришли
-		transaction.Err = err
-		return transaction
+// Пополнить баланс инвестора(его внутренний счет)
+func (investor *Investor) Recieve(transaction *models.Transaction) {
+	if transaction.Err != nil {
+		return
 	}
-	//Деньги пришли
-	to.Bill.Balance += transaction.Sum
-	transaction.Reciever = to
-	return transaction
+	investor.Bill.Balance += transaction.Transaction_sum
+	// Деньги поступили на счет. Транзакция успешна
+	transaction.Success = true
+}
+
+// Инвестор запрашивает от стартапера первод. Это задействуется, когда
+// инвестор хочет вернуть деньги инвестированные в проект
+// В этом случае подтверждать транзакцию должен стартапер
+func (investor *Investor) RequestTransaction(transaction *models.Transaction) {
+	// Бизнес-логика запроса перевода
+	if transaction.Err != nil {
+		return
+	}
+	transaction.Err = general.MakeRequest(transaction)
+}
+
+// Инвестор должен подтвердить транзакцию. Это задейстсвуется, когда
+// стартапер запросил у инвестора перевод(инвестицию в проект)
+func (investor *Investor) AcceptTransaction(transaction *models.Transaction) {
+	// Бизнес-логика подтверждения операции
+	if transaction.Err != nil {
+		return
+	}
+	// Показать запрос инвестору
+	// Получить от него явное согласие на транзакцию.
+}
+
+func (i *Investor) Topup(transaction *models.Transaction) {
+	// Бизнес-логика пополнения баланса.
+}
+
+func (i *Investor) Withdraw(transaction *models.Transaction) {
+	// Бизнес-логика снятия денег со счета
 }
