26e6c49ea8db0a89dc7a9ba1af38785d4f65acce
Maksim Donets <maks.donets.95@outlook.com>
2024-02-11 20:09:37 +0300

    enumerated interface types

--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -2,10 +2,23 @@
 // Several else are in models. Not here to avoid cycle import
 package interfaces
 
+import "github.com/udonetsm/investing/models"
+
 type Saver interface {
-	Save(any) error
+	Save(*models.Transaction)
 }
 
-type BalanceUpdater interface {
-	BalanceUpdate(any) error
+type Requester interface {
+	RequestTransaction(*models.Transaction)
+}
+
+type Accepter interface {
+	AcceptTransaction(*models.Transaction)
+}
+
+// Полный список транзакций, которые делает система.
+type Tranactioner interface {
+	TransferTransaction(*models.Transaction)
+	TopupTransaction(*models.Transaction)
+	WithdrawTransaction(*models.Transaction)
 }
