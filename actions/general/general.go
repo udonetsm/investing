e287ed9cc3e2dca6d803082197ca9e3b606d8988
Maksim Donets <maks.donets.95@outlook.com>
2024-02-11 19:56:30 +0300

    add RequestTransaction, AcceptTransaction and MakeTransaction function s for any allowed transactions and comment it

--- actions/general/general.go
+++ actions/general/general.go
@@ -2,15 +2,32 @@ package general
 
 import (
 	"github.com/udonetsm/investing/interfaces"
+	"github.com/udonetsm/investing/models"
 )
 
 // The function Saves any object into any spaces.
 // To use it, space object sould implement its method
 // Save(any)error
-func SaveSomething(saver interfaces.Saver, something any) error {
-	return saver.Save(something)
+func SaveSomething(saver interfaces.Saver, transaction *models.Transaction) {
+	saver.Save(transaction)
 }
 
-func UpdateBalance(updater interfaces.BalanceUpdater, something any) error {
-	return updater.BalanceUpdate(something)
+// Задействовать методы переданного интерфейсного типа и передать им транзакцию для обрабобтки
+func RequestTansaction(requester interfaces.Requester, transaction *models.Transaction) {
+	requester.RequestTransaction(transaction)
+}
+
+// Задействовать методы переданного интерфейсного типа и передать им транзакцию для обрабобтки
+func AcceptTransaction(accepter interfaces.Accepter, transaction *models.Transaction) {
+	accepter.AcceptTransaction(transaction)
+}
+
+// Задействовать методы переданного интерфейсного типа и передать им транзакцию для обрабобтки
+func MakeTransaction(transactioner interfaces.Tranactioner, transaction *models.Transaction) {
+	transactioner.TransferTransaction(transaction)
+}
+
+// Делает запрос плательщику
+func MakeRequest(transaction *models.Transaction) error {
+	return nil
 }
